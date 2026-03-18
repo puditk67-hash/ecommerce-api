0498f6f0a5e70656df9b62db1ddc860408abcb21
pudti <pudtik@gmail.com>
2026-03-18 23:42:27 +0700

    fix vercel path routes

--- api/routes/customers.js
+++ api/routes/customers.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db');
+const db = require('../../db');
 
 // 🔥 SEED DATA (เพิ่มอัตโนมัติถ้ายังไม่มี)
 async function seedCustomers() {
